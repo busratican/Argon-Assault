7281b80798e2f45294237f686c5a2d688277223d
Büşra GÜL <gulbusra55@gmail.com>
2022-03-13 16:07:04 +0300

    Enemies created.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
 
     ScoreBoard scoreBoard;
     [SerializeField] int pointAmount = 15;
+    [SerializeField] int hitPoint = 3;
 
-
-     void Start() 
-     {
-        scoreBoard = FindObjectOfType<ScoreBoard>();
-    }
-     void OnParticleCollision(GameObject other)
+     void Start()
     {
-        ProcessHit();
-        KillEnemy(other);
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        AddRigidBody();
     }
 
+    void AddRigidBody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.angularDrag = 0;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        hitPoint--;
+        HitEnemy();
+        if(hitPoint < 1)
+        {
+           ProcessHit();
+           KillEnemy(other);
+        }
+
+    }
+
+    private void HitEnemy()
+    {
+        GameObject hitVfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        hitVfx.transform.parent = parent;
+        //Destroy(gameObject);
+    }
     private void KillEnemy(GameObject other)
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
@@ -29,7 +51,8 @@ public class Enemy : MonoBehaviour
     }
 
     private void ProcessHit()
-    {
+    {     
         scoreBoard.IncreaseScore(pointAmount);
+  
     }
 }
